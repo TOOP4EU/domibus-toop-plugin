43008cb58f3eb7550764b0fcbcc37c7433d36fbf
Muhammet YILDIZ <myildiz83@gmail.com>
2019-07-17 14:23:58 +0300

    added copyright

--- src/main/java/eu/toop/domibus/plugin/ConnectorNotifier.java
+++ src/main/java/eu/toop/domibus/plugin/ConnectorNotifier.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2018-2019 toop.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.toop.domibus.plugin;
 
 import eu.domibus.common.ErrorCode;
@@ -14,6 +29,8 @@ import static eu.toop.domibus.plugin.Tracker.kvp;
 /**
  * This class contains the methods that are used for sending back the notifications to the
  * toop connector.
+ *
+ * @author Muhammet Yildiz
  */
 public class ConnectorNotifier implements Statics {
 
